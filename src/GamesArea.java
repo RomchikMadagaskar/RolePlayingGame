f7b265d9f12e47e09d1d28bd3edc8a989eedfd41
RomchikMadagaskar <romanm79820@gmail.com>
2024-10-09 20:03:06 +0300

    FightArena

---
+++ src/GamesArea.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GamesArea {
+   private static Scanner scanner;
+
+   public static void main(String[] args) {
+        FightArena fightArena;
+
+            //
+        //run battle
+//       public void fighStart(){
+//           fightArena(hero,monster, new WinLost(){
+//               @Override
+//               public void battleWin() {
+//                   System.out.println("Продолжим?");
+//                   try {
+//                       command(br.readline())
+//                   }
+//               }
+//           })
+//       }
+        }
+        interface WinLost{
+            void battleWin();
+            void battleLost();
+        }
+
+}
+
+
