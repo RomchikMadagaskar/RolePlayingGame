fadd5b48ad21dbc191854577c53dc18e0411eb68
RomchikMadagaskar <romanm79820@gmail.com>
2024-10-09 20:47:11 +0300

    FightArena_v2

--- src/FightArena.java
+++ src/FightArena.java
@@ -1,33 +1,46 @@
 public class FightArena {
-    public void fight(Gamer hero, Gamer monster){
-        boolean isStopBattle=false;
-        int step=0;
-        while(!isStopBattle){
+    public void fight(Gamer hero, Gamer monster,GamesArea.WinLost winLost){
+        //TODO
+        Runnable runnable=()->{
+            boolean isStopBattle = false;
+            int step = 1;
+            while (!isStopBattle) {
+                if(step%2==0) isStopBattle=nextHit(monster,hero,winLost);
+                else isStopBattle=nextHit(hero,monster,winLost);
+                try{
+                    Thread.sleep(1000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
 
-        }
+        };
+        Thread thread=new Thread(runnable);
+        thread.start();
     }
-    public void nextHit(Gamer attack, Gamer guard,GamesArea.WinLost winLost){
+    public boolean nextHit(Gamer attack, Gamer guard,GamesArea.WinLost winLost){
         int hit=attack.attack();
         int guardHp=guard.getHp()-hit;
-
         if(hit>0){
             System.out.println(String.format("%s наносит %d единиц урона...",attack.getName(),hit ));
             if(guardHp>0){
-                System.out.println(String.format("У %s осталось %в единиц здоровья...",guard.getName(),guardHp));
+                System.out.println(String.format("У %s осталось %d единиц здоровья...",guard.getName(),guardHp));
                 guard.setHp(guardHp);
             }else{
-             if(guard instanceof Hero){
-                 System.out.println("Монстры победили... Вы проиграли(....");
-                 winLost.battleLost();
-             }else {
-                 System.out.println(String.format("Призванный герой побеждает!!! Забирайте свою награду!!! Залото: %d , Опыт: %d",guard.getGold(), guard.getXp()));
-                 attack.setXp(attack.getXp()+guard.getXp());
-                 attack.setGold((attack.getGold())+guard.getGold());
-                 winLost.battleWin();
-             }
+                if(guard instanceof Hero){
+                    System.out.println("Монстры победили... Вы проиграли(....");
+                    winLost.battleLost();
+                }else {
+                    System.out.println(String.format("Призванный герой побеждает!!! Забирайте свою награду!!! Залото: %d , Опыт: %d",guard.getGold(), guard.getXp()));
+                    attack.setXp(attack.getXp()+guard.getXp());
+                    attack.setGold((attack.getGold())+guard.getGold());
+                    winLost.battleWin();
+                }
+                return true;
             }
         }else {
             System.out.println(String.format("%s промахнулся...",attack.getName()));
         }
+        return false;
     }
 }
