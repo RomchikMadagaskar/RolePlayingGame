a99c867a80dd5d4583e305210d14ebaf9db7fa95
RomchikMadagaskar <romanm79820@gmail.com>
2024-10-09 16:06:18 +0300

    ?..?

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ private static Hero hero;
 
 
 public class Main {
-    public static void main(String[] args) {
-        scanner =new Scanner(System.in);
-        printMenu();
+    public static void main(String[] args) throws IOException {
+//        scanner =new Scanner(System.in);
+//        printMenu();
 //        ShowMenu gameworld=new ShowMenu();
 //        gameworld.getStartMenu();
 //        System.out.println("bay bay");
@@ -20,65 +20,69 @@ public class Main {
 //        int n=25;
 //        int r=n/100;
 //        System.out.println(r);
-          ShowMenu menu =new ShowMenu();
-          menu.getStartMenu();
-
-    }
-    private static void printMenu() throws IOException {
-        if(hero==null){
-            System.out.println("Введите имя игрока: ");
-            hero=new Hero(scanner.nextLine());
-        }
-        System.out.println( """
-            Куды Вы хотите пойти:
-            1. К торговцу
-            2. В темный лес
-            3. На выход
-            """);
-        printSubMenu(scanner.nextLine());
-    }
-    private static void printSubMenu(String subMenu){
-        switch (subMenu){
-            case "1":
-                getMenuTrader();
-                break;
-            case "2":
-                getMenuFight();
-                break;
-            case "3": break;
-            default: break;
-        }
-
-    }
-    private static void getMenuTrader(){
-        do{
-            System.out.println("""
-                1. Начать/продолжить торговлю
-                2. Вернуться в город
-            """);
-            command=selectCommand();
-            switch (command){
-                case 1->System.out.println("Начинаем торговлю");
-                case 2->System.out.println("Заходи еще...");
-                default -> System.out.println("Введен не корректный пункт меню. Повторите ввод...");
-            }
-        }while(command!=2);
-    }
-    private static void getMenuFight(){
-        do{
-            System.out.println("""
-                1. Начать/продолжить бой
-                2. Вернуться в город
-            """);
-            command=selectCommand();
-            switch (command){
-                case 1->System.out.println("Начать/продолжить бой");
-                case 2->System.out.println("Заходи еще...");
-                default -> System.out.println("Введен не корректный пункт меню. Повторите ввод...");
-            }
-        }while(command!=2);
+//          ShowMenu menu =new ShowMenu();
+//          menu.getStartMenu();
+//
+//    }
+//    private static void printMenu() throws IOException {
+//        if(hero==null){
+//            System.out.println("Введите имя игрока: ");
+//            hero=new Hero(scanner.nextLine());
+//        }
+//        System.out.println( """
+//            Куды Вы хотите пойти:
+//            1. К торговцу
+//            2. В темный лес
+//            3. На выход
+//            """);
+//        printSubMenu(scanner.nextLine());
+//    }
+//    private static void printSubMenu(String subMenu){
+//        switch (subMenu){
+//            case "1":
+//                getMenuTrader();
+//                break;
+//            case "2":
+//                getMenuFight();
+//                break;
+//            case "3": break;
+//            default: break;
+//        }
+//
+//    }
+//    private static void getMenuTrader(){
+//        do{
+//            System.out.println("""
+//                1. Начать/продолжить торговлю
+//                2. Вернуться в город
+//            """);
+//            command=selectCommand();
+//            switch (command){
+//                case 1->System.out.println("Начинаем торговлю");
+//                case 2->System.out.println("Заходи еще...");
+//                default -> System.out.println("Введен не корректный пункт меню. Повторите ввод...");
+//            }
+//        }while(command!=2);
+//    }
+//    private static void getMenuFight(){
+//        do{
+//            System.out.println("""
+//                1. Начать/продолжить бой
+//                2. Вернуться в город
+//            """);
+//            command=selectCommand();
+//            switch (command){
+//                case 1->System.out.println("Начать/продолжить бой");
+//                case 2->System.out.println("Заходи еще...");
+//                default -> System.out.println("Введен не корректный пункт меню. Повторите ввод...");
+//            }
+//        }while(command!=2);
 
     }
 
 
 }
+
+//public void main() {
+//}
+
